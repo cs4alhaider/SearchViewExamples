bf7b97dac8db9440b70714fdb187a2cbae6234eb
Abdullah Alhaider <cs.alhaider@gmail.com>
2024-02-26 14:58:11 +0300

    Initial Commit

---
+++ SearchViewExamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchViewExamples
+//
+//  Created by 16Root19 on 26/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchViewExamples/SearchViewExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewExamplesApp.swift
+//  SearchViewExamples
+//
+//  Created by 16Root19 on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SearchViewExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
